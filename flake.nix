98201d9d93af10d8caf8367e53ab7641fd9adcc6
nasadorian <narek.asadorian@gmail.com>
2024-02-22 16:40:16 -0800

    Fix crate structure; Add num_traits; Impl checked traits

--- flake.nix
+++ flake.nix
@@ -7,23 +7,27 @@
     flake-parts.url  = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-parts, ... }:
-    flake-parts.perSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+  outputs = inputs@{ nixpkgs, rust-overlay, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = { system, pkgs, ... }:  {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
+        devShells.default = with pkgs; mkShell {
           buildInputs = [
             openssl
             pkg-config
-            rust-bin.beta.latest.default
+            rust-bin.stable.latest.complete  
           ];
         };
-      }
-    );
+      };
+    };
 }
