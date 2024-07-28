d25cd31d69766612e1bb6c3f9a49d08adade5be2
0xPlish <plish@hylo.so>
2024-07-27 18:30:50 -0700

    Drop bits field from UFixValue variants for simplicity

--- src/fix_value.rs
+++ src/fix_value.rs
@@ -1,57 +1,46 @@
-use crate::typenum::{Integer, Unsigned};
+use crate::typenum::{Integer, U10};
 use crate::Fix;
-use anchor_lang::prelude::{borsh, AnchorDeserialize, AnchorSerialize, ErrorCode, InitSpace};
+use anchor_lang::prelude::{borsh, AnchorDeserialize, AnchorSerialize, InitSpace};
 use paste::paste;
 
 macro_rules! impl_fix_value {
     ($sign:ident, $bits:expr) => {
         paste! {
-           /// A dumbed down version of `Fix` with a concrete `bits` type.
-           /// Only intended for serialized storage in Solana accounts.
+           /// A value-space `Fix` where base is always 10 and bits are a concrete type.
+           /// Intended for serialized storage in Solana accounts where generics won't work.
             #[derive(PartialEq, Eq, Copy, Clone, Debug, AnchorSerialize, AnchorDeserialize, InitSpace)]
             pub struct [<$sign FixValue $bits>] {
                 pub bits: [<$sign:lower $bits>],
-                pub base: u8,
                 pub exp: i8,
             }
 
             impl [<$sign FixValue $bits>] {
-                pub fn new(bits: [<$sign:lower $bits>], base: u8, exp: i8) -> Self {
-                    Self { bits, base, exp }
+                pub fn new(bits: [<$sign:lower $bits>], exp: i8) -> Self {
+                    Self { bits, exp }
                 }
             }
 
-            impl<Bits, Base, Exp> From<Fix<Bits, Base, Exp>> for [<$sign FixValue $bits>]
+            impl<Bits, Exp> From<Fix<Bits, U10, Exp>> for [<$sign FixValue $bits>]
             where
                 Bits: Into<[<$sign:lower $bits>]>,
-                Base: Unsigned,
                 Exp: Integer,
             {
-                fn from(fix: Fix<Bits, Base, Exp>) -> Self {
+                fn from(fix: Fix<Bits, U10, Exp>) -> Self {
                     Self {
                         bits: fix.bits.into(),
-                        base: Base::to_u8(),
                         exp: Exp::to_i8(),
                     }
                 }
             }
 
-            impl<Bits, Base, Exp> TryInto<Fix<Bits, Base, Exp>> for [<$sign FixValue $bits>]
+            impl<Bits, Exp> From<[<$sign FixValue $bits>]> for Fix<Bits, U10, Exp>
             where
                 Bits: From<[<$sign:lower $bits>]>,
-                Base: Unsigned,
                 Exp: Integer,
             {
-                type Error = anchor_lang::error::Error;
-                fn try_into(self) -> anchor_lang::Result<Fix<Bits, Base, Exp>> {
-                    let base = Base::to_u8();
-                    let exp = Exp::to_i8();
-                    if base == self.base && exp == self.exp {
-                        Ok(Fix::new(Bits::from(self.bits)))
-                    } else {
-                        Err(ErrorCode::AccountDidNotDeserialize.into())
-                    }
-                }
+              fn from(value: [<$sign FixValue $bits>]) -> Fix<Bits, U10, Exp> {
+                Fix::new(value.bits.into())
+              }
             }
         }
     };
@@ -82,14 +71,14 @@ mod tests {
                 fn [<roundtrip_into_ $sign:lower $bits>]() -> Result<()> {
                     let start = Kilo::new([<69 $sign:lower $bits>]);
                     let there: [<$sign FixValue $bits>] = start.into();
-                    let back: Kilo<[<$sign:lower $bits>]> = there.try_into()?;
-                    assert_eq!(there, [<$sign FixValue $bits>]::new(69, 10, 3));
+                    let back: Kilo<[<$sign:lower $bits>]> = there.into();
+                    assert_eq!(there, [<$sign FixValue $bits>]::new(69, 3));
                     Ok(assert_eq!(start, back))
                 }
 
                 #[test]
                 fn [<roundtrip_serialize_ $sign:lower $bits>]() -> Result<()> {
-                    let start = [<$sign FixValue $bits>]::new(20, 10, -2);
+                    let start = [<$sign FixValue $bits>]::new(20, -2);
                     let bytes = to_vec(&start)?;
                     let back = AnchorDeserialize::deserialize(&mut bytes.as_slice())?;
                     Ok(assert_eq!(start, back))
