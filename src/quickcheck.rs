538ac69ef53d396395e380b230e7f41969d1f829
0xPlish <plish@hylo.so>
2024-08-26 20:18:44 -0700

    remove quickcheck

--- src/quickcheck.rs
+++
@@ -1,8 +0,0 @@
-use crate::aliases::decimal::UFix64;
-use quickcheck::Arbitrary;
-
-impl<Exp: 'static> Arbitrary for UFix64<Exp> {
-    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-        UFix64::new(u64::arbitrary(g))
-    }
-}
